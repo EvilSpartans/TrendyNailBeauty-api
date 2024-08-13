0d24d1c54f5267a545e806c87eb6953d2f8613bd
Alexis Moren <alexismoren62@hotmail.com>
2024-08-13 17:37:17 +0200

    fix auth api

--- src/Events/JwtCreatedListener.php
+++ src/Events/JwtCreatedListener.php
@@ -4,9 +4,18 @@ namespace App\Events;
 
 use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class JwtCreatedListener
 {
+
+    public function __construct(
+        private NormalizerInterface $normalizer,
+        private JWTTokenManagerInterface $JWTManager
+    ) {
+    }
+
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
     {
         $data = $event->getData();
@@ -16,13 +25,8 @@ class JwtCreatedListener
             return;
         }
 
-        $data['data'] = [
-            'roles' => $user->getRoles(),
-            'id' => $user->getId(),
-            'username' => $user->getUsername(),
-            'email' => $user->getEmail(),
-        ];
-
+        $results = ['token' => $this->JWTManager->create($user), 'data' => $user];
+        $data = $this->normalizer->normalize($results, 'json', ['groups' => ['getUsers']]);
         $event->setData($data);
     }
 }
